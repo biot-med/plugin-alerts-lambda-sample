1d54d6bc35627b066d310f67e2d47f2915e11048
Ori-Roll <orir@biot-med.com>
2023-02-13 09:34:12 +0200

    file ordering

--- src/perform/perform.js
+++ src/perform/perform.js
@@ -1,5 +1,5 @@
 import { genericSuccessResponse } from "../utils/index.js";
-import { getPatientAlertResponse } from "./apiCalls.js";
+import { getPatientAlertResponse } from "../BEService/apiCalls.js";
 
 export const perform = async (data, token, traceId) => {
   // -----------------------------------------------------------------------------------------
@@ -7,10 +7,6 @@ export const perform = async (data, token, traceId) => {
   // TODO: ORI: ADD EXPLANATION HERE !
 
   console.log("3. Got data: " + data)
-
-  const callExampleResponse = await getPatientAlertResponse(token, traceId);
-
-  console.log("4. Got response: " + callExampleResponse)
 
   // -----------------------------------------------------------------------------------------
   return genericSuccessResponse(traceId);
