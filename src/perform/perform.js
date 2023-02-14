19c56de7a2818739526792425f59f787fa96b582
Ori-Roll <orir@biot-med.com>
2023-02-14 10:53:26 +0200

    perform cleanup

--- src/perform/perform.js
+++ src/perform/perform.js
@@ -5,22 +5,12 @@ import { getAlert } from '../alertsService/alertsService.js';
 
 export const perform = async (data, token, traceId) => {
   console.log("--------------    In perform    ---------------");
-  
-  console.log("3. Got data: " + data)
 
   // TODO: ORI: ADD EXPLANATION HERE !
 
   const { measurement, patientId } = extractEntityData(data);
 
-
-  console.log("extracted measurementsBiomarker measurement ", measurement);
-  console.log("extracted patientId = ", patientId);
-  
-
   const requiredAlert = getAlert(measurement)
-
-  console.log("extracted requiredAlert ", requiredAlert)
-  console.log(requiredAlert)
   
   return genericSuccessResponse(traceId);
 };
