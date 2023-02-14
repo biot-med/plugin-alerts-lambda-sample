3474ae8167c054ba4260d09d31691f55d44df7df
Ori-Roll <orir@biot-med.com>
2023-02-14 14:33:06 +0200

    setAlert in perform

--- src/perform/perform.js
+++ src/perform/perform.js
@@ -1,7 +1,7 @@
 import { genericSuccessResponse } from "../utils/index.js";
 import { extractEntityData } from '../utils/extractEntityData.js';
-import { getAlert } from '../alertsService/alertsService.js';
-
+import { getRequiredAlert, setAlert } from '../alertsService/alertsService.js';
+import { BIOT_ALERT_TEMPLATE_ID } from '../constants.js'
 
 export const perform = async (data, token, traceId) => {
   console.log("--------------    In perform    ---------------");
@@ -10,7 +10,9 @@ export const perform = async (data, token, traceId) => {
 
   const { measurement, patientId } = extractEntityData(data);
 
-  const requiredAlert = getAlert(measurement)
+  const requiredAlert = getRequiredAlert(measurement)
   
+  const alert = await setAlert(requiredAlert, patientId, BIOT_ALERT_TEMPLATE_ID, token, traceId)
+
   return genericSuccessResponse(traceId);
 };
